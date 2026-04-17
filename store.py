66512f04f2821bc6816cb656b56d40d8b1b98c5c
D J <jayadithya.dhurwey@gmail.com>
2026-04-18 03:58:14 +0530

    removed the comments

--- store.py
+++ store.py
@@ -33,26 +33,4 @@ def writetoJSON():
             json.dump(L,file,indent=4)
     except Exception as e:
         print(f'error in writetoJSON {e}')
-#json.dump(obj,file)
-#json.load(file)
-
-#def readfromJSON():
-    #try:
- #       if does_it_exist():
- #           with open(filename,'r') as file:
- #               List = json.load(file)
- #               Todo.L = CustomEncoder.decode_list(List)
- #       else:
- #           with open(filename,'w') as file:
- #               Empty = []
-  #              json.dump(Empty,file,indent=4)
- #   except Exception as e:
- #       print(f'ERROR in readfromJSON {e}')
-
-#def writetoJSON():
-   # try:
-   #     List = CustomEncoder.serialize_list()
-    #    with open(filename,'w') as file:
-   #         json.dump(List,file,indent=4)
- #   except Exception as e:
- #       print(f'ERROR in Writetojson as {e}')"""        
+  
